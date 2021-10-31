7ffd92ab8f2a08821ca20735499c027e86c29998
ErikinBC <alla_hack@hotmail.com>
2021-10-31 19:20:32 -0400

    equations updated

--- _rmd/extra_FI/explore_binom.py
+++ _rmd/extra_FI/explore_binom.py
@@ -1,13 +1,40 @@
 import os
 import numpy as np
 import pandas as pd
+import plotnine as pn
 from scipy import stats
 from scipy.stats import norm
-from funs_explore import binom_experiment, gg_save
+from funs_explore import BPFI, gg_save
 
 dir_base = os.getcwd()
 dir_figures = os.path.join(dir_base, 'figures')
 assert os.path.exists(dir_figures), 'no figures folder found'
+
+######################################
+# ---- (1) CHECK FI CALCULATION ---- #
+
+n = 100000
+pi2 = 0.6
+pi1 = 0.5
+alpha = 0.05
+
+sim1 = BPFI(n=n, pi1=pi1, pi2=pi2, alpha=alpha)
+sim1.dgp(nsamp=10000)
+sim1.fi()
+sim1.mu_f
+sim1.df_fi.query('reject==1').mean()
+
+
+gtit='Negative values imply reverse FI\n$\\pi_d=0.1,\\pi_1=0.5,n=100$'
+gg_fi_approx = (pn.ggplot(sim1.df_fi,pn.aes(x='fia',y='fi')) + 
+    pn.theme_bw() + pn.labs(x='BPFI-approx',y='BPFI-exact') + 
+    pn.ggtitle(gtit) + pn.geom_point() + 
+    pn.geom_abline(intercept=0,slope=1,color='blue',linetype='--'))
+gg_save('gg_fi_approx.png',dir_figures,gg_fi_approx,6,4)
+
+
+
+
 
 ###########################################
 # ---- (1) ESTIMATE QUALITY OF POWER ---- #
@@ -18,8 +45,6 @@ n_seq = [25, 100, 500, 2000]
 pi1_seq = [0.05, 0.25, 0.5]
 pid_seq = [0, 0.01, 0.05, 0.1]
 n_perm = len(n_seq) * len(pi1_seq) * len(pid_seq)
-
-
 
 holder = []
 jj = 0
--- _rmd/extra_FI/funs_explore.py
+++ _rmd/extra_FI/funs_explore.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import pandas as pd
 from scipy.stats import norm
 
 """
@@ -24,11 +25,10 @@ def gg_save(fn,fold,gg,width,height):
 
 
 #  Define a binomial experiment class for equal sample sizes
-class binom_experiment():
-    def __init__(self, n, pi1, pi2=None, alpha=0.05, agresti=True):
+class BPFI():
+    def __init__(self, n, pi1, pi2=None, alpha=0.05):
         self.n = n
         self.alpha = alpha
-        self.agresti = agresti
         self.pi1 = pi1
         self.pi2 = pi2
         if self.pi2 is None:
@@ -38,30 +38,46 @@ class binom_experiment():
         if self.pid > 0:
             self.null = False
         # Calculate the oracle power
-        self.q_alpha = norm.ppf(1-alpha)
-        num = np.sqrt(2*self.pi1*(1-self.pi1))*self.q_alpha-np.sqrt(n)*self.pid
+        self.t_a = norm.ppf(1-alpha)
+        num = np.sqrt(2*self.pi1*(1-self.pi1))*self.t_a-np.sqrt(n)*self.pid
         den = np.sqrt(self.pid + self.pi1*(2-self.pi1) - (self.pi1 + self.pid)**2 )
         self.power = 1 - norm.cdf(num / den)
-        
+
+        # Calculate the expected value of the FI
+        se_f = np.sqrt(2*self.n*self.pi1*(1-self.pi1))
+        self.mu_f = self.n*(self.pi2-self.pi1) - self.t_a*se_f
+        self.mu_f += se_f*norm.pdf(-self.mu_f/se_f)/norm.cdf(self.mu_f/se_f)
+
+
+    @staticmethod
+    def pval_bp(s1, s2, n):
+        pid_hat = (s2 - s1)/n
+        pi0_hat = (s2 + s1) / (2*n)
+        se_null = np.sqrt( 2 * pi0_hat*(1-pi0_hat) / n )
+        z = pid_hat/se_null
+        pval = 1 - norm.cdf(z)
+        return pval
+
     def dgp(self, nsamp, seed=None):
         if seed is not None:
             np.random.seed(seed)
         # Draw data
         self.s1 = np.random.binomial(self.n, self.pi1, nsamp)
         self.s2 = np.random.binomial(self.n, self.pi2, nsamp)
-        # Calculate sample statistic and variance
-        if self.agresti:
-            n_tilde = self.n + self.q_alpha**2
-            self.pi1_hat = (self.s1 + 0.5*self.q_alpha**2) / n_tilde
-            self.pi2_hat = (self.s2 + 0.5*self.q_alpha**2) / n_tilde
-        else:
-            self.pi1_hat = self.s1 / self.n
-            self.pi2_hat = self.s2 / self.n
-        self.pid_hat = self.pi2_hat - self.pi1_hat
-        self.pi_hat = (self.pi1_hat + self.pi2_hat) / 2
-        # Calculate statistic under the null
-        self.se_null = np.sqrt( 2 * self.pi_hat*(1-self.pi_hat) / self.n )
-        self.z = self.pid_hat/self.se_null
-        self.pval = 1 - norm.cdf(self.z)
-        self.reject = np.where(self.pval < self.alpha, 1, 0)
+        self.pi0_hat = (self.s2 + self.s1) / (2*self.n)
+        self.se_null = np.sqrt( 2 * self.pi0_hat*(1-self.pi0_hat) / self.n )
+        self.pval = BPFI.pval_bp(self.s1, self.s2, self.n)
+        self.reject = self.pval < self.alpha
+
+    def fi(self):
+        # Calculate the FI
+        a = (2*self.n+self.t_a**2)
+        b = 2*(self.t_a**2*(self.s1-self.n)-2*self.n*self.s1)
+        c = self.s1*(2*self.n*self.s1+self.t_a**2*(self.s1-2*self.n))
+        s2hat1 = (-b + np.sqrt(b**2 - 4*a*c))/(2*a)
+        fi1 = self.s2 - s2hat1
+        # Calculation FI(approx)
+        s2hat2 = (self.s1 + np.sqrt(2*self.n*self.pi0_hat*(1-self.pi0_hat))*self.t_a)
+        fi2 = self.s2 - s2hat2
+        self.df_fi = pd.DataFrame({'reject':self.reject,'fi':fi1, 'fia':fi2})
 
