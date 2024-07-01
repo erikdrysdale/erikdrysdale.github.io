1a770f13799def6fe62f3de56e4fb36156760fe1
ErikinBC <erikinwest@gmail.com>
2024-06-30 22:27:52 -0400

    post ready for primetime

--- _rmd/fix_kramdown.py
+++ _rmd/fix_kramdown.py
@@ -13,14 +13,16 @@ This script will do the following:
 1. Replace the ![png]({file}_files/{name}.png) to <center><p><img src="/figures/{file}.png" width="100%"></p></center>
 2. REPLACE \label{eq:X} with \tag{1}\label{eq:X}, allowing \eqref{eq:X} to work properly
 3. REPLACE ALL $...$ WITH \\(...\\), allowing kramdown to render the math properly
-
+4. Write a version of the file in ~/output/{YYYY}-{mm}-{dd}-{title}.md
 
 Example usage
 > python3 -m _rmd.fix_kramdown --dmin 2024-06-30
 
+Once you're happy with the result, move the file from the ~/output/ folder into the ~/posts folder
+
 """
 
-# (i) Process arguments
+# (i) Process arguments (e.g. `dmin='2024-06-30'`)
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--dmin', default=None, type=str, help='Minimum date to consider')
@@ -61,9 +63,8 @@ def get_user_confirmation():
             # Place the code to continue your script here
             break
         elif user_input == 'n':
-            print("Exiting the script...")
             # Place the code to exit your script here
-            break
+            sys.exit("Exiting the script...")
         else:
             print("Invalid input. Please enter 'y' or 'n'.")
 
@@ -73,22 +74,31 @@ get_user_confirmation()
 # (v) Loop over all posts to be converted
 for ii, fn in enumerate(fn_posts):
     print('Post: %s (%i of %i)' % (fn, ii+1, len(fn_posts)))
-    # sys.exit('tmp stop')
 
     # (a) Load the file
     connection = open(os.path.join(dir_posts, fn), 'r')
     lines = pd.Series(connection.readlines())
     connection.close()
 
-    # (b)
+    # (b) Replace the markdown image paths
     # Replace the ![png]({file}_files/{name}.png) to <center><p><img src="/figures/{file}.png" width="100%"></p></center>
-
+    idx_md_png = lines.str.contains('![png]',regex=False)
+    lines[idx_md_png] = lines[idx_md_png].str.replace("![png](", '<center><p><img src="', regex=False)
+    lines[idx_md_png] = lines[idx_md_png].str.replace('.png)', '.png" width="100%"></p></center>', regex=False)
+    # Clean of the "place"
+    tmp_path = lines[idx_md_png].str.split('"', n=2, regex=False, expand=True)
+    tmp_path_fn = tmp_path.iloc[:,1].str.split('/', n=2, regex=False, expand=True).iloc[:,1]
+    tmp_path = tmp_path.iloc[:,0] + '"/figures/' + tmp_path_fn + '"' + tmp_path.iloc[:,2]
+    # Over-write
+    lines[idx_md_png] = tmp_path.copy()
+    
     # (c) Find the equations
-    eq_labels = lines[lines.str.contains('\\\\label\\{eq\\:')]
-    if len(eq_labels) > 0:
+    idx_eq_labels = lines.str.contains('\\\\label\\{eq\\:')
+    if idx_eq_labels.sum() > 0:
         print('Has label/eqref')
+        eq_labels = lines[idx_eq_labels]
         tick = 0
-        for kk, eq in eq_labels.iteritems():
+        for kk, eq in eq_labels.items():
             if eq.split('\\label')[0][-1] == ' ':
                 print('Needs tag')
                 tick += 1
@@ -97,9 +107,9 @@ for ii, fn in enumerate(fn_posts):
             else:
                 print('Already has tag')
     # Remove double $ dollars temporarily
-    lines = lines.str.replace('\\${2}', '@@')
+    lines = lines.str.replace('\\${2}', '@@', regex=True)
     # Replace currency dollar signs
-    lines = lines.str.replace('\\\\\\$', '~~')
+    lines = lines.str.replace('\\\\\\$', '~~', regex=True)
 
     should_skip = False
     for jj, line in enumerate(lines):
@@ -124,10 +134,10 @@ for ii, fn in enumerate(fn_posts):
             print('Original: %s\nNew: %s' % (line, tmp))
             lines[jj] = tmp
     # Under the blocks
-    lines = lines.str.replace('@@','$$')
-    lines = lines.str.replace('~~', '\\\\$')
+    lines = lines.str.replace('@@','$$', regex=True)
+    lines = lines.str.replace('~~', '\\\\$', regex=True)
     # Now write
-    path_w = os.path.join(dir_output,fn)
+    path_w = os.path.join(dir_output, fn)
     copy = open(path_w, 'w')
     for line in lines:
         copy.write(line)
